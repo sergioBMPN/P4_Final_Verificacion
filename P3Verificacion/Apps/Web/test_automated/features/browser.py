d83af6b8a37143b5f00b7aa3feb88da91ad27599
sergioBMPN <sergioBMPN@users.noreply.github.com>
2018-06-05 20:52:35 +0200

    prueba firefox

--- P3Verificacion/Apps/Web/test_automated/features/browser.py
+++ P3Verificacion/Apps/Web/test_automated/features/browser.py
@@ -18,7 +18,8 @@ def open_drivers():
 
 def get_chrome():
     #driver = webdriver.Chrome(r'C:\Users\migue\Documents\Verificacion programas\chromedriver.exe')
-    driver = webdriver.Chrome(r '~/proyectos/P4_Final_Verificacion/P3Verificacion/Apps/Web/test_automated/features/chromedriver')
+    #driver = webdriver.Chrome(r'~/proyectos/P4_Final_Verificacion/P3Verificacion/Apps/Web/test_automated/features/chromedriver')
+    driver = webdriver.Firefox()
     return driver
 
 def close_drivers():
